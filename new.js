f4dac45b7dfd17b95f4feae9d7a922698d9a9a97
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2022-10-29 01:31:12 +0500

    Add

--- new.js
+++ new.js
@@ -26,4 +26,31 @@ var exampleObj = {
     prop3: 5
 };
 exampleObj.prop3 = 43;
-console.log(exampleObj);
+var evh = {
+    name: "Eddie",
+    active: false,
+    albums: [1984, 5150, "OU812"]
+};
+var jp = {
+    name: "aliraza",
+    active: true,
+    albums: ["I", "II", "IV"]
+};
+var greetGuitarist = function (guitarist) {
+    if (guitarist.name) {
+        return "Hello ".concat(guitarist.name.toUpperCase(), "!");
+    }
+    return "Hello!";
+};
+console.log(greetGuitarist(jp));
+// Enums
+// "Unlike most TypeScript features, Enums are not a type-level addition to JavaScript but something added to the language and runtime."
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 1] = "U";
+    Grade[Grade["D"] = 2] = "D";
+    Grade[Grade["C"] = 3] = "C";
+    Grade[Grade["B"] = 4] = "B";
+    Grade[Grade["A"] = 5] = "A";
+})(Grade || (Grade = {}));
+console.log(Grade.D);
--- new.ts
+++ new.ts
@@ -37,4 +37,47 @@ const exampleObj = {
 };
 
 exampleObj.prop3 = 43;
-console.log(exampleObj);
+
+type Guitarist = {
+  name?: string;
+  active: boolean;
+  albums: (string | number)[];
+};
+
+let evh: Guitarist = {
+  name: "Eddie",
+  active: false,
+  albums: [1984, 5150, "OU812"],
+};
+let jp: Guitarist = {
+  name: "aliraza",
+  active: true,
+  albums: ["I", "II", "IV"],
+};
+const greetGuitarist = (guitarist: Guitarist) => {
+  if (guitarist.name) {
+    return `Hello ${guitarist.name.toUpperCase()}!`;
+  }
+  return "Hello!";
+};
+
+console.log(greetGuitarist(jp));
+// Enums
+// "Unlike most TypeScript features, Enums are not a type-level addition to JavaScript but something added to the language and runtime."
+
+enum Grade {
+  U = 1,
+  D,
+  C,
+  B,
+  A,
+}
+
+console.log(Grade.D);
+
+interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  role: "admin" | "user" | "super-admin";
+}
